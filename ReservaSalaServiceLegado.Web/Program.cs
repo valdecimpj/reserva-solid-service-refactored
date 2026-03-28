facb4564dae0ef56699fb4f614064ca018b3c87e
valdecimpj <valdecimpj@gmail.com>
2026-03-28 23:54:27 +0000

    feat: improve swagger

--- ReservaSalaServiceLegado.Web/Program.cs
+++ ReservaSalaServiceLegado.Web/Program.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Mvc;
 using ReservaSalaServiceLegado.Core.Repository;
 using ReservaSalaServiceLegado.Core.Service;
@@ -5,6 +6,17 @@ using ReservaSalaServiceLegado.Core.UseCases.RentRoom;
 using ReservaSalaServiceLegado.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
+
+builder.Services.Configure<JsonOptions>(options =>
+{
+    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
+
 builder.Services.AddScoped<RentRoomHandler>();
 builder.Services.AddScoped<IEmailService, EmailService>();
 builder.Services.AddScoped<IPaymentValidatorService, PaymentValidatorService>();
@@ -21,13 +33,17 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.RoutePrefix = string.Empty;
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Rent Room API V1");
+    });
 }
 
-app.MapPost("/rent-room", async ([FromServices] RentRoomHandler handler, [FromBody] RentRoomRequest request) =>
+app.MapPost("/v1/rent-room", async ([FromServices] RentRoomHandler handler, [FromBody] RentRoomRequest request) =>
 {
     var message = await handler.Handle(request);
-    return Results.Json(new {Message = message});
+    return Results.Json(new { Message = message });
 })
 .WithName("RentRoom");
 
